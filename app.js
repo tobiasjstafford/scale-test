077cec4cde6c9e90e642c07c2e8b5d7673ff6649
Toby Stafford <tobiasjstafford@gmail.com>
2014-10-13 22:22:39 -0500

    Adds Everything!
    Adds the whole node_modules folder because I had to do some craziness
    (such as including a forked verison of node-hid for now) to get this
    working. Will clean up when it's not TEN THIRTY AT NIGHT!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var $ = require('jquery');
-var HID = require('node-hid');
+var HID = require('node-hid-fixed');
 
 $(function() {
   $('#get-devices').on('click', function() {
@@ -12,5 +12,27 @@ $(function() {
     });
 
     $devices.html(html);
+
+    $('#read-data').show();
+    $('#output').hide();
+  });
+
+  $('#read-data').on('click', function() {
+    var path = $('#devices').val();
+
+    var device = new HID.HID(path);
+
+    device.read(function(err, data) {
+      if (err) {
+        data = err;
+      }
+
+      var html = '';
+      $.each(data, function(idx, el) {
+        html += 'byte: ' + el + ', ';
+      });
+
+      $('#output').val($('#output').val() + '\n' + html).show();
+    });
   });
 });
